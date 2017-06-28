7f6b0dd6b7b9c98594265908c50865f58980fc07
sonia.garg <sonia.garg@metadesignsolutions.co.uk>
2017-06-28 19:19:45 +0530

    initial commit

---
+++ game.js
@@ -0,0 +1,57 @@
+
+// Setting 
+// var upperBound = 
+
+
+
+// var boudary_x = 0px;  
+
+
+window.onload = function(){
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+
+
+function moveLeft(){
+	
+}
+function moveRight(){
+	
+}
+function moveUp(){
+	
+}
+function moveDown(){
+	
+}
+function Stop(){
+
+}
+function start(){
+
+}
+
+setInterval(draw, 700);
+
+}
